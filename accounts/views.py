2623eae83844588160dd8bf7eaf380db4669f733
Ubuntu <ubuntu@ip-172-31-18-173.ap-southeast-1.compute.internal>
2019-10-31 03:31:21 +0000

    Only show relevant links depending on if the user has logged in or not

--- accounts/views.py
+++ accounts/views.py
@@ -18,7 +18,22 @@ def logout(request):
 This is login function
 """
 def login(request):
-    form = UserLoginForm()
-    return render(request, 'accounts/login.template.html', {
-        'form':form
-    })
+    if request.method == "POST":
+        # populate the login form with the user's input
+        login_form = UserLoginForm(request.POST)
+        
+        # if the input to the login form is valid
+        if login_form.is_valid():
+            # use auth.authenticate to check if the
+            # user name and password matches
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+            #if there is a user (meaning, user is not None)
+            if user:
+                auth.login(user=user, request=request)
+                return redirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+        return render(request, 'accounts/login.template.html', {
+            'form':form
+        })
