eaafba1085e05d2eaaeb21f485f9629d011225ce
Ubuntu <ubuntu@ip-172-31-18-173.ap-southeast-1.compute.internal>
2019-11-01 03:44:20 +0000

    Added a new model for Item

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ItemModel(models.Model):
+    # What are the fields inside this model (i.e, the attributes)
+    name = models.CharField(max_length=30, blank=False)
+    done = models.BooleanField(blank=False)
+    
+    def __str__(self):
+        return self.name
