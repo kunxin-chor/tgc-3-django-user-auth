0b8c838b78c658f6653a0217b884c2746bde648a
Ubuntu <ubuntu@ip-172-31-18-173.ap-southeast-1.compute.internal>
2019-11-05 07:12:50 +0000

    Updated requirements.txt

--- ProjectManagement/settings.py
+++ ProjectManagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,12 +80,14 @@ WSGI_APPLICATION = 'ProjectManagement.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.parse(os.environ['DATABASE_URL'])}
 
 
 # Password validation
@@ -169,3 +172,4 @@ UPLOADCARE = {
     'pub_key': UPLOADCARE_PUBLIC_KEY,
     'secret':UPLOADCARE_SECRET_KEY
 }
+
